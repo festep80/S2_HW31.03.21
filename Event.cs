8415e886ad799ecff1e1b5b6e856c58ebdc96fcb
festep80 <74856291+festep80@users.noreply.github.com>
2021-04-01 17:57:15 +0300

    v0.0.2

--- Event.cs
+++ Event.cs
@@ -27,6 +27,57 @@ namespace S2_HW31._03._21
         string name;
         uint people;
         string place;
+        DateTime date;
+        static readonly uint counter;
+
+        public Event(string name = "NoName", uint people = 0, string place = "Unknown", DateTime date = default)
+        {
+            this.Name = name;
+            this.People = people;
+            this.Place = place;
+            if(date==default)
+            {
+                this.Date = DateTime.Today;
+            }
+            else
+            {
+                this.Date = date;
+            }
+            
+        }
+        public string Name 
+        {
+            get => this.name;
+            set 
+            {
+                if(!string.IsNullOrWhiteSpace(value))
+                {
+                    this.name = value;
+                }
+                else
+                {
+                    this.name = "Unknown";
+                }
+            }
+        }
+        public uint People { get; set; }
+        public string Place { get; set; }
+        public DateTime Date 
+        { 
+            get => this.date;
+            set 
+            {
+                if(Date < DateTime.Now)
+                {
+                    this.date = default;
+                }
+            }
+        }
+        public override string ToString()
+        {
+            return $"\n{counter}\nName: {Name}\nPeople: {People}\nPlace: {Place}\nDate: {Date}\n";
+        }
         
     }
+    
 }
--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@
 namespace S2_HW31._03._21
 {
     class Program
+        
     {
         static void Main(string[] args)
         {
